e12b37c67c4b14341e19168d76de560ec7fee326
oun1982 <oun1982@gmail.com>
2018-02-21 11:42:52 +0700

    Signed-off-by: oun1982 <oun1982@gmail.com>

--- JavaApplication01/src/javaapplication01/JavaApplication01.java
+++ JavaApplication01/src/javaapplication01/JavaApplication01.java
@@ -10,12 +10,13 @@ package javaapplication01;
  * @author Pongsakon
  */
 public class JavaApplication01 {
+      
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("HelloGit");
     }
     
 }
